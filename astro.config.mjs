28278bbdfabbe5de3d352bf80027faa99b5f4bd5
Andreas Loew <andreas@codeandweb.com>
2024-07-09 11:39:44 +0200

    Building

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,45 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Recipes',
-					autogenerate: { directory: 'recipes' },
-				},
-				{
-					label: 'Resources',
-					autogenerate: { directory: 'resources' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'My Docs',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+	  customCss: [
+		  // Path to your Tailwind base styles:
+		  './src/tailwind.css',
+	  ],
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        slug: 'guides/example'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }, {
+      label: 'Recipes',
+      autogenerate: {
+        directory: 'recipes'
+      }
+    }, {
+      label: 'Resources',
+      autogenerate: {
+        directory: 'resources'
+      }
+    }]
+  }), tailwind({
+	  // Disable the default base styles:
+	  applyBaseStyles: false,
+  })]
 });
