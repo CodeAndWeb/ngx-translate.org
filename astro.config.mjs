51ca5718b34b7093f240704ad48efe4872661e11
Andreas Loew <andreas@codeandweb.com>
2024-07-10 12:12:07 +0200

    Sitemap, removed trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator';
-import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+import {rehypeHeadingIds} from '@astrojs/markdown-remark'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ngx-translate.org',
+    trailingSlash: 'never',
     markdown: {
         rehypePlugins: [
             rehypeHeadingIds, [
@@ -20,49 +21,53 @@ export default defineConfig({
             ],
         ]
     },
-    integrations: [starlight({
-        title: 'ngx-translate',
-        plugins: [starlightLinksValidator()],
-        logo: {
-            src: "./src/assets/ngx-translate.svg"
-        },
-        components: {
-
-        },
-        social: {
-            github: 'https://github.com/ngx-translate/core'
-        },
-        customCss: [
-            // Path to your Tailwind base styles:
-            './src/tailwind.css',
-        ],
-        sidebar: [
-            {
-                label: 'Getting started',
-                autogenerate: {
-                    directory: '01-getting-started'
-                }
+    integrations: [
+        sitemap({
+            filter: (page) => page !== 'https://ngx-translate.org/imprint' && page !== 'https://ngx-translate.org/privacy-notice',
+        }),
+        starlight({
+            title: 'ngx-translate',
+            plugins: [starlightLinksValidator()],
+            logo: {
+                src: "./src/assets/ngx-translate.svg"
             },
-            {
-                label: 'Reference',
-                autogenerate: {
-                    directory: '02-reference'
-                }
+            components: {
+                PageFrame: "./src/components/PageFrameWithFooter.astro"
             },
-            {
-                label: 'Recipes',
-                autogenerate: {
-                    directory: '03-recipes'
-                }
+            social: {
+                github: 'https://github.com/ngx-translate/core'
             },
-            {
-                label: 'Resources',
-                autogenerate: {
-                    directory: '04-resources'
-                }
-            }]
-    }), tailwind({
-        // Disable the default base styles:
-        applyBaseStyles: false,
-    })]
+            customCss: [
+                // Path to your Tailwind base styles:
+                './src/tailwind.css',
+            ],
+            sidebar: [
+                {
+                    label: 'Getting started',
+                    autogenerate: {
+                        directory: '01-getting-started'
+                    }
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: {
+                        directory: '02-reference'
+                    }
+                },
+                {
+                    label: 'Recipes',
+                    autogenerate: {
+                        directory: '03-recipes'
+                    }
+                },
+                {
+                    label: 'Resources',
+                    autogenerate: {
+                        directory: '04-resources'
+                    }
+                }]
+        }), tailwind({
+            // Disable the default base styles:
+            applyBaseStyles: false,
+        })]
 });
