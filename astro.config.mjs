bf2290d4cc832f712948c4de88afa9a32cf4b719
Andreas Loew <andreas@codeandweb.com>
2025-07-04 14:38:24 +0200

    Upgrade to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,160 +4,164 @@ import starlightLinksValidator from 'starlight-links-validator';
 import {rehypeHeadingIds} from '@astrojs/markdown-remark';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 import matomo from 'astro-matomo';
 import rehypeMermaid from 'rehype-mermaid'
 import starlightVersions from "starlight-versions";
+
+import tailwindcss from "@tailwindcss/vite";
 
 //import markdown from "astro/dist/vite-plugin-markdown/index.js";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ngx-translate.org',
-    trailingSlash: 'never',
-    markdown: {
-        rehypePlugins: [
-            rehypeHeadingIds,
-            rehypeMermaid,
-            [rehypeAutolinkHeadings, {
-            // Wrap the heading text in a link.
-            behavior: 'wrap'
-        }]]
-    },
-    redirects: {
-      //  '/': "/getting-started/installation/"
-    },
-    integrations: [
-        sitemap({
-            filter: page => page !== 'https://ngx-translate.org/imprint' && page !== 'https://ngx-translate.org/privacy-notice'
-        }),
-        matomo({
-            enabled: import.meta.env.PROD,
-            // Only load in production
-            host: "https://analytics.codeandweb.com/",
-            setCookieDomain: "*.codeandweb.com",
-            siteId: 3,
-            heartBeatTimer: 5,
-            disableCookies: true,
-            debug: false
-        }),
-        starlight({
-            title: 'ngx-translate',
-            plugins: [
-                starlightLinksValidator({
-                    exclude: ["/"]
-                }),
-                starlightVersions({
-                    current: {
-                        label: "v16"
-                    },
-                    versions: [
-                        {
-                            slug: 'v15',
-                            label: 'v15',
-                        },
-                    ],
-                }),
-            ],
-            logo: {
-                src: "./src/assets/ngx-translate.svg"
-            },
-            favicon: "/images/favicon.svg",
-            head: [
-                // ICO-Favicon als Fallback für Safari hinzufügen
-                {
-                    tag: 'link',
-                    attrs: {
-                        rel: 'icon',
-                        href: "/images/favicon.ico",
-                        sizes: '32x32',
-                    },
-                },
-            ],
-            components: {
-                PageFrame: "./src/components/PageFrameWithFooter.astro"
-            },
-            social: {
-                github: 'https://github.com/ngx-translate/core'
-            },
-            editLink: {
-                baseUrl: 'https://github.com/CodeAndWeb/ngx-translate.org/edit/main/',
-            },
-            customCss: [
-                // Path to your Tailwind base styles:
-                './src/tailwind.css'],
-            sidebar: [{
-                label: 'Start here',
-                items: [
-                    {
-                        label: "Getting started",
-                        link: "/"
-                    },
-                    {
-                        label: "Installation",
-                        link: "/getting-started/installation"
-                    },
-                    {
-                        label: "Translation files",
-                        link: "/getting-started/translation-files"
-                    },
-                    {
-                        label: "Translating your components",
-                        link: "/getting-started/translating-your-components"
-                    },
-                    {
-                        label: "Migration guide v15 → v16",
-                        link: "/getting-started/migration-guide"
-                    }
-                ]
-            }, {
-                label: 'Reference',
-                items: [
-                    {
-                        label: "Concepts",
-                        link: "/reference/concepts"
-                    },
-                    {
-                        label: "Configuration",
-                        link: "/reference/configuration"
-                    },
-                    {
-                        label: "TranslateService API",
-                        link: "/reference/translate-service-api"
-                    },
-                    {
-                        label: "TranslateLoader API",
-                        link: "/reference/translate-loader-api"
-                    },
-                    {
-                        label: "TranslateCompiler API",
-                        link: "/reference/translate-compiler-api"
-                    },
-                    {
-                        label: "TranslateParser API",
-                        link: "/reference/translate-parser-api"
-                    },
-                    {
-                        label: "MissingTranslationHandler API",
-                        link: "/reference/missing-translation-handler-api"
-                    },
-                ]
-            }, {
-                label: 'Recipes',
-                autogenerate: {
-                    directory: '30-recipes'
-                }
-            }, {
-                label: 'Resources',
-                autogenerate: {
-                    directory: '40-resources'
-                }
-            }]
-        }),
-        tailwind({
-            // Disable the default base styles:
-            applyBaseStyles: false
-        })
-    ]
+  site: 'https://ngx-translate.org',
+  trailingSlash: 'never',
+
+  markdown: {
+      rehypePlugins: [
+          rehypeHeadingIds,
+          rehypeMermaid,
+          [rehypeAutolinkHeadings, {
+          // Wrap the heading text in a link.
+          behavior: 'wrap'
+      }]]
+  },
+
+  redirects: {
+    //  '/': "/getting-started/installation/"
+  },
+
+  integrations: [
+      sitemap({
+          filter: page => page !== 'https://ngx-translate.org/imprint' && page !== 'https://ngx-translate.org/privacy-notice'
+      }),
+      matomo({
+          enabled: import.meta.env.PROD,
+          // Only load in production
+          host: "https://analytics.codeandweb.com/",
+          setCookieDomain: "*.codeandweb.com",
+          siteId: 3,
+          heartBeatTimer: 5,
+          disableCookies: true,
+          debug: false
+      }),
+      starlight({
+          title: 'ngx-translate',
+          plugins: [
+              starlightLinksValidator({
+                  exclude: ["/"]
+              }),
+              starlightVersions({
+                  current: {
+                      label: "v16"
+                  },
+                  versions: [
+                      {
+                          slug: 'v15',
+                          label: 'v15',
+                      },
+                  ],
+              }),
+          ],
+          logo: {
+              src: "./src/assets/ngx-translate.svg"
+          },
+          favicon: "/images/favicon.svg",
+          head: [
+              // ICO-Favicon als Fallback für Safari hinzufügen
+              {
+                  tag: 'link',
+                  attrs: {
+                      rel: 'icon',
+                      href: "/images/favicon.ico",
+                      sizes: '32x32',
+                  },
+              },
+          ],
+          components: {
+              PageFrame: "./src/components/PageFrameWithFooter.astro"
+          },
+          social: [
+              { icon: 'github', label: 'GitHub', href: 'https://github.com/ngx-translate/core' }
+          ],
+          editLink: {
+              baseUrl: 'https://github.com/CodeAndWeb/ngx-translate.org/edit/main/',
+          },
+          customCss: [
+              // Path to your Tailwind base styles:
+              './src/tailwind.css'],
+          sidebar: [{
+              label: 'Start here',
+              items: [
+                  {
+                      label: "Getting started",
+                      link: "/"
+                  },
+                  {
+                      label: "Installation",
+                      link: "/getting-started/installation"
+                  },
+                  {
+                      label: "Translation files",
+                      link: "/getting-started/translation-files"
+                  },
+                  {
+                      label: "Translating your components",
+                      link: "/getting-started/translating-your-components"
+                  },
+                  {
+                      label: "Migration guide v15 → v16",
+                      link: "/getting-started/migration-guide"
+                  }
+              ]
+          }, {
+              label: 'Reference',
+              items: [
+                  {
+                      label: "Concepts",
+                      link: "/reference/concepts"
+                  },
+                  {
+                      label: "Configuration",
+                      link: "/reference/configuration"
+                  },
+                  {
+                      label: "TranslateService API",
+                      link: "/reference/translate-service-api"
+                  },
+                  {
+                      label: "TranslateLoader API",
+                      link: "/reference/translate-loader-api"
+                  },
+                  {
+                      label: "TranslateCompiler API",
+                      link: "/reference/translate-compiler-api"
+                  },
+                  {
+                      label: "TranslateParser API",
+                      link: "/reference/translate-parser-api"
+                  },
+                  {
+                      label: "MissingTranslationHandler API",
+                      link: "/reference/missing-translation-handler-api"
+                  },
+              ]
+          }, {
+              label: 'Recipes',
+              autogenerate: {
+                  directory: '30-recipes'
+              }
+          }, {
+              label: 'Resources',
+              autogenerate: {
+                  directory: '40-resources'
+              }
+          }]
+      })
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
 ;
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,9 @@
 import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
-import { docsVersionsSchema } from 'starlight-versions/schema'
-
+import { docsLoader } from '@astrojs/starlight/loaders';
+import { docsVersionsLoader } from 'starlight-versions/loader';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-	versions: defineCollection({ type: 'data', schema: docsVersionsSchema() }),
+	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	versions: defineCollection({ loader: docsVersionsLoader() }),
 };
